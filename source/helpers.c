03c4c4f5b2aca6004f6c3e8ece58346b8e764f27
Antoni Strasz <antonstrasz@gmail.com>
2025-05-03 16:19:41 +0200

    Add error handling

---
+++ source/helpers.c
@@ -0,0 +1,13 @@
+#include <helpers.h>
+
+
+
+void error_exit(const char *msg){
+     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+     exit(EXIT_FAILURE);
+}
+
+void msg_exit(const char *msg){
+    fprintf(stderr, "Exit: %s\n", msg);
+    exit(EXIT_FAILURE);
+}
---
+++ source/helpers.h
@@ -0,0 +1,16 @@
+// Antoni Strasz 339096
+
+#ifndef HELPERS_H
+#define HELPERS_H
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void error_exit(const char *msg);
+void msg_exit(const char *msg);
+
+
+
+#endif
