780fed02f726e75879545c39d6e6f9936fc883e6
Antoni Strasz <antonstrasz@gmail.com>
2025-05-03 19:50:22 +0200

    Add sendto wrapper

--- source/wrappers.h
+++ source/wrappers.h
@@ -6,9 +6,11 @@
 
 void safe_inet_pton(int af, const char *src, void *dst);
 int safe_socket(int domain, int type, int protocol);
+void safe_sendto(int fd, const void *buf, size_t n, int flags, const struct sockaddr *addr, socklen_t addr_len);
 
 int safe_atoi(char *src);
 
 FILE *safe_fopen(const char *filename, const char *mode);
+
 
 #endif
