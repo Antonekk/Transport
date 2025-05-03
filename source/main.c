15d9e41beb649f0095532a56550ba13ab9f6e954
Antoni Strasz <antonstrasz@gmail.com>
2025-05-03 19:50:52 +0200

    send to server

--- source/main.c
+++ source/main.c
@@ -7,7 +7,12 @@
 
 #define MAX_PORT 65535
 #define MAX_FILESIZE 10000000
-#define MAX_DATAGRAM_DATA_LENGTH 1000
+
+#define MAX_REQUEST_LEN 50
+
+#define MAX_RESPONSE_LEN 1100
+#define MAX_RESPONSE_BODY_LEN 1000
+
 
 int main(int argc, char *argv[]){
     
@@ -17,14 +22,14 @@ int main(int argc, char *argv[]){
     char *ip_addr = argv[1];
     int port_number = safe_atoi(argv[2]);
     char *filename = argv[3];
-    int size = safe_atoi(argv[4]);
+    int to_recive = safe_atoi(argv[4]);
 
     if (port_number < 0 || port_number > MAX_PORT){
         msg_exit("Port number outside of valid range");
     }
 
 
-    printf("transport ip %s | port %d | filename %s | size %d\n", ip_addr, port_number, filename, size);
+    printf("transport ip %s | port %d | filename %s | size %d\n", ip_addr, port_number, filename, to_recive);
     int socket_fd = safe_socket(AF_INET, SOCK_DGRAM, 0);
 
 
@@ -34,8 +39,18 @@ int main(int argc, char *argv[]){
     server_addr.sin_port = htons(port_number);
     safe_inet_pton(AF_INET, ip_addr, &server_addr.sin_addr);
 
+    FILE *file = safe_fopen(filename, "w+");
     
+    char request_buf[MAX_REQUEST_LEN];
+    char response_buf[MAX_RESPONSE_LEN];
 
+    int start = 0;
+    while (start < to_recive){
+        int rq_length = to_recive - start < MAX_RESPONSE_BODY_LEN ? to_recive - start : MAX_RESPONSE_BODY_LEN;
+        snprintf(request_buf, sizeof(request_buf), "GET %d %d\n", start, rq_length);
+
+        sendto(socket_fd, request_buf, strlen(request_buf), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    }
 
 
     return EXIT_SUCCESS;
